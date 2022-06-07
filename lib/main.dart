9cbd0438aae6143dec05189a2b06b1d693688420
akshat <akshat.goyal@oodles.io>
2022-06-07 11:12:27 +0530

    peerconnection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+//import 'package:permission_handler/permission_handler.dart';
+
+//import 'package:webrtc/get_user_media.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -29,17 +33,136 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _localrenderer = RTCVideoRenderer();
+  final _remoterenderer = RTCVideoRenderer();
+  final sdpController = TextEditingController();
+
+  @override
+  void dispose() {
+    _localrenderer.dispose();
+    _remoterenderer.dispose();
+    sdpController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    initRenderers();
+    _getUserMedia();
+    super.initState();
+  }
+
+  initRenderers() async {
+    await _localrenderer.initialize();
+    await _remoterenderer.initialize();
+  }
+
+  _getUserMedia() async {
+    final Map<String, dynamic> mediaConstrains = {
+      'audio': false,
+      'video': {
+        'facingmode': 'user',
+      }
+    };
+    MediaStream stream =
+        await navigator.mediaDevices.getUserMedia(mediaConstrains);
+
+    _localrenderer.srcObject = stream;
+    // _localrenderer.mirror = true;
+    RTCVideoView(
+      _localrenderer,
+      mirror: true,
+    );
+  }
+
+  SizedBox videoRenderers() => SizedBox(
+      height: 300,
+      child: Row(children: [
+        Flexible(
+          child: Container(
+            key: const Key('local'),
+            margin: const EdgeInsets.all(5.0),
+            decoration: const BoxDecoration(color: Colors.black),
+            child: RTCVideoView(_localrenderer),
+          ),
+        ),
+        Flexible(
+          child: Container(
+            key: const Key('local'),
+            margin: const EdgeInsets.all(5.0),
+            decoration: const BoxDecoration(color: Colors.black),
+            child: RTCVideoView(_remoterenderer),
+          ),
+        ),
+      ]));
+  Row offerandAnsButtons() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            onTap: null,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey,
+              child: const Text("Offer"),
+            ),
+          ),
+          GestureDetector(
+            onTap: null,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey,
+              child: const Text("Answer"),
+            ),
+          ),
+        ],
+      );
+
+  Padding sdpCandidateTF() => Padding(
+        padding: const EdgeInsets.all(15),
+        child: TextField(
+          controller: sdpController,
+          keyboardType: TextInputType.multiline,
+          maxLines: 4,
+          maxLength: TextField.noMaxLength,
+        ),
+      );
+
+  Row sdpCandidateButtons() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            onTap: null, //_setRemoteDescription,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey,
+              child: const Text("Set remote desc"),
+            ),
+          ),
+          GestureDetector(
+            onTap: null, //_setCandidate,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey,
+              child: const Text("Set candidate"),
+            ),
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("sup")],
-        ),
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: Column(children: [
+          videoRenderers(),
+          offerandAnsButtons(),
+          sdpCandidateTF(),
+          sdpCandidateButtons(),
+        ]),
       ),
     );
   }
